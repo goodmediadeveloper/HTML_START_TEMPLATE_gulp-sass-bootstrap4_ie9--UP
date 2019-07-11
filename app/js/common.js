66d2abcbda925a5e1010addc83949e92f53d9938
Dzmitry Prokharau <goodmediadeveloper@gmail.com>
2019-07-11 12:51:25 +0300

    first commit

---
+++ app/js/common.js
@@ -0,0 +1,223 @@
+$(function () {
+
+    $('body').fadeIn();
+
+    // Custom JS
+
+    //CAROUSEL OWL
+
+    $(document).ready(function () {
+        $('#owl-events-1').owlCarousel({
+            loop: true, //Зацикливаем слайдер
+            margin: 50, //Отступ от элемента справа в 50px
+            dots: false,
+            nav: true, //Отключение навигации
+            navText: ['<div class="owl-nav__prev"></div>', '<div class="owl-nav__next"></div>'],
+            autoplay: false, //Автозапуск слайдера
+            smartSpeed: 1000, //Время движения слайда
+            autoplayTimeout: 2000, //Время смены слайда
+            responsive: { //Адаптивность. Кол-во выводимых элементов при определенной ширине.
+                0: {
+                    items: 1,
+                    nav: false,
+                    margin: 0
+                },
+                992: {
+                    items: 1,
+                    nav: false,
+                    margin: 0
+                },
+                1000: {
+                    items: 1,
+                    margin: 0,
+
+                }
+            }
+        });
+    });
+
+    $(document).ready(function () {
+        $('#owl-events-2').owlCarousel({
+            loop: true, //Зацикливаем слайдер
+            margin: 50, //Отступ от элемента справа в 50px
+            dots: false,
+            nav: true, //Отключение навигации
+            navText: ['<div class="owl-nav__prev"></div>', '<div class="owl-nav__next"></div>'],
+            autoplay: false, //Автозапуск слайдера
+            smartSpeed: 1000, //Время движения слайда
+            autoplayTimeout: 2000, //Время смены слайда
+            responsive: { //Адаптивность. Кол-во выводимых элементов при определенной ширине.
+                0: {
+                    items: 1,
+                    nav: false,
+                    margin: 0
+                },
+                992: {
+                    items: 1,
+                    margin: 20,
+                    nav: true,
+                },
+                1000: {
+                    items: 2,
+                    margin: 20,
+                    nav: true,
+                }
+            }
+        });
+    });
+
+    $(document).ready(function () {
+        $('#owl-events-3').owlCarousel({
+            loop: true, //Зацикливаем слайдер
+            margin: 50, //Отступ от элемента справа в 50px
+            dots: false,
+            nav: true, //Отключение навигации
+            navText: ['<div class="owl-nav__prev"></div>', '<div class="owl-nav__next"></div>'],
+            autoplay: false, //Автозапуск слайдера
+            smartSpeed: 1000, //Время движения слайда
+            autoplayTimeout: 2000, //Время смены слайда
+            responsive: { //Адаптивность. Кол-во выводимых элементов при определенной ширине.
+                0: {
+                    items: 1,
+                    nav: false,
+                    margin: 0
+                },
+                992: {
+                    items: 1,
+                    nav: false,
+                    margin: 20
+                },
+                1000: {
+                    items: 2,
+                    margin: 20
+                }
+            }
+        });
+    });
+    //END CAROUSEL OWL
+
+
+    // timer
+    $(document).ready(function () {
+        function get_timer() {
+
+            var newDate = "October 6,2019 00:00";
+            ////////////////////////////////////
+            ////////////////////////////////////
+
+            var targetDate = new Date(newDate);
+
+            var date = new Date();
+
+            var timer = targetDate - date;
+
+            if (targetDate > date) {
+
+                var day = parseInt(timer / (60 * 60 * 1000 * 24));
+
+                if (day < 10) {
+                    day = '0' + day;
+                }
+
+                day = day.toString();
+
+                var hour = parseInt(timer / (60 * 60 * 1000)) % 24;
+
+                if (hour < 10) {
+                    hour = '0' + hour;
+                }
+
+                hour = hour.toString();
+
+                var min = parseInt(timer / (1000 * 60)) % 60;
+
+                if (min < 10) {
+                    min = '0' + min;
+                }
+
+                min = min.toString();
+
+
+                var sec = parseInt(timer / 1000) % 60;
+
+                if (sec < 10) {
+                    sec = '0' + sec;
+                }
+
+                sec = sec.toString();
+
+                $('.day').text(day);
+                $('.hour').text(hour);
+                $('.min').text(min);
+                $('.sec').text(sec);
+
+                setTimeout(get_timer, 1000);
+            } else {
+            }
+
+        }
+
+        get_timer();
+    });
+    // end timer
+
+    //smooth scrollspy
+    // $(document).ready(function () {
+    //     var navHeight;
+    //     if ($('.header-top')) {
+    //         navHeight = ($('.header-top').css('height').slice(0, -2)) * (-1);
+    //     }
+    //     if ($("#nav")) {
+    //         $("#nav").scrollspy({
+    //             activeClass: 'active-li',
+    //             offset: navHeight,
+    //             animate: true,
+    //             duration: 500,
+    //
+    //         });
+    //         window.onscroll = function () {
+    //             if (document.scrollingElement.scrollTop === 0) {
+    //                 $('nav ul li').removeClass('active-li');
+    //             }
+    //
+    //         }
+    //     }
+    //
+    // });
+    //end smooth scrollspy
+
+
+    var wow = new WOW(
+        {
+            boxClass:     'wow',      // animated element css class (default is wow)
+            animateClass: 'animated', // animation css class (default is animated)
+            offset:       0,          // distance to the element when triggering the animation (default is 0)
+            mobile:       true,       // trigger animations on mobile devices (default is true)
+            live:         true,       // act on asynchronously loaded content (default is true)
+            callback:     function(box) {
+                // the callback is fired every time an animation is started
+                // the argument that is passed in is the DOM node being animated
+            },
+            scrollContainer: null // optional scroll container selector, otherwise use window
+        }
+    );
+    wow.init();
+
+    $('.lazy').Lazy({
+        // your configuration goes here
+        scrollDirection: 'vertical',
+        effect: 'fadeIn',
+        visibleOnly: true,
+        onError: function(element) {
+            console.log('error loading ' + element.data('src'));
+        }
+    });
+
+
+
+
+
+});///
+
+
+
---
+++ gulpfile.js
@@ -0,0 +1,184 @@
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	sass = require('gulp-sass'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	cleanCss = require('gulp-clean-css'),
+	rename = require('gulp-rename'),
+	prefixer = require('gulp-autoprefixer'),
+	notify = require('gulp-notify'),
+	browserSync = require('browser-sync'),
+	reload = browserSync.reload,
+	rigger = require('gulp-rigger'),
+	sourcemaps = require('gulp-sourcemaps'),
+	rimraf = require('rimraf'),
+	uncss = require('gulp-uncss'),
+	uncss = require('gulp-uncss'),
+	cssmin = require('gulp-csso'),
+	imageMin = require('gulp-imagemin'),
+	pngQuant = require('imagemin-pngquant'),
+	rsync = require('gulp-rsync');
+
+
+
+var path = {
+	//папка куда складываются готовые файлы
+	dist: {
+		html: 'dist/',
+		js: 'dist/js/',
+		css: 'dist/css/',
+		img: 'dist/images/',
+		fonts: 'dist/fonts/',
+        video: 'dist/video/'
+
+	},
+	//папка откуда брать файлы
+	app: {
+		html: 'app/[^_]*.html',
+		js: 'app/js/*.js',
+		css: 'app/css/*css',
+		style: 'app/sass/*.sass',
+		img: 'app/images/**/*.{jpg,jpeg,png,svg}',
+		fonts: 'app/fonts/**/*.*',
+        video: 'app/video/**/*.*'
+	},
+	//указываем после измененя каких файлов нужно действовать
+	watch: {
+		html: 'app/**/*.html',
+		js: 'app/js/**/*.js',
+		style: 'app/**/*.{media-sass-sass,sass}',
+		img: 'app/images/**/*.*',
+		video: 'app/video/**/*.*',
+		fonts: 'app/fonts/**/*.*'
+	},
+	clean: './dist'
+};
+
+var configServer = {
+	server: {
+		baseDir: "./dist" //из какой папки показывать
+	},
+	notify: false,
+	host: 'localhost',
+	port: 9000,
+};
+
+html = () => {
+	return gulp.src(path.app.html)
+		.pipe(rigger())
+		.pipe(gulp.dest(path.dist.html))
+		.pipe(reload({
+			stream: true
+		}));
+};
+
+styles = () => {
+	return gulp.src(path.app.style)
+		// .pipe(sourcemaps.init())
+		.pipe(sass({
+			outputStyle: 'expanded'
+		}).on("error", notify.onError()))
+		//comment
+		// .pipe(uncss({
+		// 	html: ['dist/*.html']
+		// }))
+		// .pipe(rename({ suffix: '.min', prefix : '' }))
+		//comment
+		.pipe(prefixer({
+			browsers: ['last 15 versions']
+		}))
+		.pipe(cssmin())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(cleanCss({
+			level: {
+				1: {
+					specialComments: 0
+				}
+			}
+		})) // Opt., comment out when debugging
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(path.dist.css))
+		.pipe(reload({
+			stream: true
+		}));
+};
+
+js = () => {
+	return gulp.src([
+			'app/libs/jquery/dist/jquery.min.js',
+			'app/libs/owl.carousel/dist/owl.carousel.min.js',
+			'app/libs/scrollspy/scrollspy.min.js',
+			'app/libs/wow/dist/wow.min.js',
+       		'app/libs/jquery-lazy/jquery.lazy.min.js',
+			path.app.js
+		])
+		.pipe(rigger())
+		.pipe(sourcemaps.init())
+		.pipe(concat('scripts.min.js'))
+		// .pipe(uglify())
+		//.pipe(sourcemaps.write())
+		.pipe(gulp.dest(path.dist.js))
+		.pipe(reload({
+			stream: true
+		}));
+};
+
+images = () => {
+	return gulp.src(path.app.img)
+		// .pipe(imageMin({
+		// 	progressive: true,
+		// 	use: [pngQuant()],
+		// 	interlaced: true
+		// }))
+		.pipe(gulp.dest(path.dist.img))
+		.pipe(reload({stream: true}));
+};
+
+videos = () => {
+	return gulp.src(path.app.video)
+		.pipe(gulp.dest(path.dist.video))
+		.pipe(reload({stream: true}));
+};
+
+fonts = () => {
+    return gulp.src(path.app.fonts)
+        .pipe(gulp.dest(path.dist.fonts))
+};
+
+clean = (cb) => {
+	return rimraf(path.clean, cb);
+};
+
+webServer = () => {
+	return browserSync(configServer);
+};
+
+watch = () => {
+	gulp.watch(path.watch.html, html);
+	gulp.watch(path.watch.style, styles);
+	gulp.watch(path.watch.js, js);
+	gulp.watch(path.watch.img, images);
+	gulp.watch(path.watch.fonts, fonts);
+	gulp.watch(path.watch.video, videos);
+};
+
+var start = gulp.series(clean, gulp.parallel(webServer, watch, html, styles, js, fonts, images, videos));
+gulp.task('default', start);
+
+
+// gulp.task('rsync', function() {
+// 	return gulp.src('app/**')
+// 	.pipe(rsync({
+// 		root: 'app/',
+// 		hostname: 'username@yousite.com',
+// 		destination: 'yousite/public_html/',
+// 		// include: ['*.htaccess'], // Includes files to deploy
+// 		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
+// 		recursive: true,
+// 		archive: true,
+// 		silent: false,
+// 		compress: true
+// 	}))
+// });
